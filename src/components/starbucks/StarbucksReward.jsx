b8483fdae99775c662ac7870a457729a03422f14
Aman Relan <aman.relan-ext@jci.com>
2024-01-10 22:35:00 +0530

    Neutrinos starbucks assignment header

---
+++ src/components/starbucks/StarbucksReward.jsx
@@ -0,0 +1,9 @@
+const StarbucksReward = () => {
+  return (
+    <div style={{ backgroundColor: "#1e3932", width: "100%", height: "40px" }}>
+      <div style={{ color: "#fff" }}>Starbucks Rewards</div>
+    </div>
+  );
+};
+
+export default StarbucksReward;
